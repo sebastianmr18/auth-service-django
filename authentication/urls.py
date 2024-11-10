4995a02a9a9a23adc29795a7fb19aa88a4bf4537
sebastianmr18 <juan.munoz.rojas@correounivalle.edu.co>
2024-11-09 21:10:11 -0500

    fix: removing coreapi

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,6 @@ from rest_framework.documentation import include_docs_urls
 from .views import RegisterUserView, RegisterWarehouseAssistantView, LoginView
 
 urlpatterns = [
-    path('docs/', include_docs_urls(title='API Documentation')),
     path('register/user', RegisterUserView.as_view(), name='register_user'),
     path('register/warehouse-assistant', RegisterWarehouseAssistantView.as_view(), name='register_warehouse_assistant'),
     path('login', LoginView.as_view(), name='login'),
